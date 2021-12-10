2fff75a67bd4a94a43721fa8fc4b334273540c44
Alexandre Eugene <you@example.com>
2021-12-10 21:26:15 +0100

    correction probleme afichage mobil plus amelioration design

--- src/Components/Home/NavBar/index.js
+++ src/Components/Home/NavBar/index.js
@@ -1,7 +1,6 @@
 import React from 'react'
-import { NavLink } from 'react-router-dom'
 import './navBar.scss'
-import { useEffect } from 'react'
+
 
 export const NavBar = () => {
 
--- src/Components/Home/index.js
+++ src/Components/Home/index.js
@@ -16,7 +16,6 @@ export const Home = () => {
 
             <div className="home__down">
                 <NavBar className="navBar" />
-               
                 <Picture className="picture" />
             </div>
             <div className="home__container__presentation">
